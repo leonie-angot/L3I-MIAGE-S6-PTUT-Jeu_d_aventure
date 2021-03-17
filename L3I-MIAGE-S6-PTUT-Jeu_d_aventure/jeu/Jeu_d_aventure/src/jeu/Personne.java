2c0c79e202d18ccc6c17272cf47d82809b095d47
Dahour-Tarik <77979359+Dahour-Tarik@users.noreply.github.com>
2021-03-17 11:14:51 +0100

    Update Personne.java

--- L3I-MIAGE-S6-PTUT-Jeu_d_aventure/jeu/Jeu_d_aventure/src/jeu/Personne.java
+++ L3I-MIAGE-S6-PTUT-Jeu_d_aventure/jeu/Jeu_d_aventure/src/jeu/Personne.java
@@ -14,25 +14,12 @@ package jeu;
 public class Personne {
 	
 	private String namePersonne;
-	Objet premierObjet;
-	Objet deuxiemeObjet;
-	private Zone localisation;
+
 	
-	public Personne(String nom, Objet o, Zone l) {
+	public Personne(String nom) {
 		namePersonne = nom;
-		premierObjet = o;
-		localisation = l;
 	}
-	
-	
-	public Personne(String nom,Zone l, Objet o1, Objet o2) {
-		this(nom,o1,l);
-		deuxiemeObjet = o2;
-	}
-	
-	public Zone getZone() {
-		return localisation;
-	}
+
 	
 	public String getName() {
 		return namePersonne;
